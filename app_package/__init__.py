89f68a7a01ddbffdb723d03256efba3af8e32578
sammy <sammy@sammykingx.com.ng>
2023-02-23 16:29:28 +0000

    fixed the csrf issue

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.config['SECRET KEY'] = 'ec2e5161526a1d4668d7df8ad8af8e38b07c9e5e'
+app.config['SECRET_KEY'] = 'ec2e5161526a1d4668d7df8ad8af8e38b07c9e5e'
+
 
 import app_package.views
--- app_package/views.py
+++ app_package/views.py
@@ -1,19 +1,35 @@
 from app_package import app
 from app_package.forms import login, register
-from flask import render_template
+from flask import render_template, url_for
 
 
 @app.route('/', strict_slashes=False)
 @app.route('/home', strict_slashes=False)
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route('/about', strict_slashes=False)
+def about():
+    return render_template('about.html')
+
+@app.route('/blog', strict_slashes=False)
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact', strict_slashes=False)
+def contact():
+    return render_template('contact.html')
 
 @app.route('/login', strict_slashes=False)
 def signin():
     lg_form = login()
-    return render_template('login.html')
+    return render_template('login.html', form=lg_form)
 
-@app.route('/register', strict_slashes=False)
+@app.route('/register', strict_slashes=False, methods = ['GET', 'POST'])
 def signup():
     reg_form = register()
-    return '<h1>Resister Page</h1>'
+    return '<h2>Resister Page</h2>'
+
+@app.route('/shop', strict_slashes=False)
+def shop():
+    return '<h2>This is the shop page</h2>'
